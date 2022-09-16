974ab8627000800b94b44fc28e74dbdb59c79a06
Henrique Gonçalves <110692092+Henriquegcosta@users.noreply.github.com>
2022-09-16 12:27:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 
 var musics = document.getElementById("pagina");
+var rotateee = document.getElementById("paginex");
+
+
+
+function rotatee(){
+    rotateee.classList.toggle("rotate");
+}
+
 
 function abrir_musicas(){
 
     musics.classList.toggle("exibir");
 
+}
+function abrir_musicas2(){
+    musics.classList.toggle("exibir2");
+}
+function abrir_musicas3(){
+    musics.classList.toggle("exibir3");
+}
+function abrir_musicas4(){
+    musics.classList.toggle("exibir4");
+}
+
+
+
+
+function chamar_pagina(pagina){
+    musics.innerHTML = pagina;
 }
---
+++ music.js
@@ -0,0 +1,2 @@
+var music = document.getElementById("music");
+music.volume = 0.1;
